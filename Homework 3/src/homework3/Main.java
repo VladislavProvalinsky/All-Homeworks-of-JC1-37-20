bcbe5cd526cfeb273d64def0c888d412d7f4efaa
VladislavProvalinsky <62176491+VladislavProvalinsky@users.noreply.github.com>
2020-03-26 00:58:27 +0300

    Update Main.java

--- Homework 3/src/homework3/Main.java
+++ Homework 3/src/homework3/Main.java
@@ -11,19 +11,19 @@ public class Main {
 //        int [] arr2 = {0,1,2,3,4,5,6,7,8,0};
 //        int [] arr3 = {0,1,2,3,5,5,5,7,8,0};
 //        int [] arr4 = {1,1,1,1,1,1,1,1,1,0};
-        int [] arr5 = {7,9,4,5,3,2,0,6,1,8};
-//        Scanner in = new Scanner(System.in);
-//        System.out.print("Введите количество элементов вашего массива: ");
-//        int n = in.nextInt();
-//        int [] arr = new int [n];
-//        System.out.print("Введите элементы вашего массива: ");
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = in.nextInt();
-//        }
+//        int [] arr5 = {7,9,4,5,3,2,0,6,1,8};
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите количество элементов вашего массива: ");
+        int n = in.nextInt();
+        int [] arr = new int [n];
+        System.out.print("Введите элементы вашего массива: ");
+        for (int i = 0; i < arr.length; i++) {
+           arr[i] = in.nextInt();
+        }
         ArraysSort ob = new ArraysSort();
-        System.out.println("Ваш массив: "+ Arrays.toString(arr5));
-        ob.shakerSort(arr5);
-        System.out.println("Отсортированный массив: "+ Arrays.toString(arr5));
-        System.out.println("Массив без дубликатов: "+ Arrays.toString(ob.deleteDublicat(arr5)));
+        System.out.println("Ваш массив: "+ Arrays.toString(arr));
+        ob.shakerSort(arr);
+        System.out.println("Отсортированный массив: "+ Arrays.toString(arr));
+        System.out.println("Массив без дубликатов: "+ Arrays.toString(ob.deleteDublicat(arr)));
     }
 }
