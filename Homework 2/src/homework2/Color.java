e1b5a19da00c86446d2a85c5ef5a0c72ed61c48d
VladislavProvalinsky <62176491+VladislavProvalinsky@users.noreply.github.com>
2020-04-30 22:10:05 +0300

    Add files via upload

--- Homework 2/src/homework2/Color.java
+++ Homework 2/src/homework2/Color.java
@@ -1,19 +1,33 @@
+//ДЗ 2
+// 1. Создать класс Color. Класс должен содержать:
+// 1.0 Все константы должны быть приватными, все методы должны быть публичны, конструктор должен быть публичными. Статических методов быть не должно.
+// 1.1 Создать конструктор который будет принимать параметр int. Туда мы будем передавать номер цвета.
+// 1.2 Внутри класса создать константы цветов радуги которые будут содержать номера цветов
+// (http://cyclowiki.org/wiki/%D0%9A%D0%B0%D0%B6%D0%B4%D1%8B%D0%B9_%D0%BE%D1%85%D0%BE%D1%82%D0%BD%D0%B8%D0%BA_%D0%B6%D0%B5%D0%BB%D0%B0%D0%B5%D1%82_%D0%B7%D0%BD%D0%B0%D1%82%D1%8C,_%D0%B3%D0%B4%D0%B5_%D1%81%D0%B8%D0%B4%D0%B8%D1%82_%D1%84%D0%B0%D0%B7%D0%B0%D0%BD).
+// Например RED = 1;
+// 1.3 Создать методы getNumber() и getName(). Метод getNumber должен вернуть номер цвета который мы передали в конструктор.
+// Метод getName должен возвращать название цвета ввиде строки на русском, если цвет нам не известен возвращать строку "неизвестно" .
+// В методе getName должен использоваться switch и константы объявленные при выполнении первого задания. Внимание никакой печати
+// в консоль внутри класс Color быть не должно.
+// 2. В отдельном классе, создать мэйн метод. Внутри данного метода создать экземпляр Color. В конструктор передать число
+// 3. Далее вывести в консоль номер цвета и его названия использую результат выполнения методов getNumber и getName.
+
 package homework2;
 
 public class Color {
-    private int Red;
-    private int Orange;
-    private int Yellow;
-    private int Green;
-    private int Blue;
-    private int Siniy;
-    private int Purple;
-    private int UnknownColor;
-    private int ColorNumber;
-    private String ColorName;
 
-    public Color(int ColorNumber) {
-        this.ColorNumber = ColorNumber;
+    private final int RED = 1;
+    private final int ORANGE = 2;
+    private final int YELLOW = 3;
+    private final int GREEN = 4;
+    private final int BLUE = 5;
+    private final int SINIY = 6;
+    private final int PURPLE = 7;
+
+    private int ColorNumber;
+
+    public Color(int number) {
+        this.ColorNumber = number;
     }
 
     public int getNumber() {
@@ -23,37 +37,21 @@ public class Color {
     public String getName() {
         switch (ColorNumber) {
             case 1:
-                Red = ColorNumber;
-                ColorName = "Красный";
-                break;
+                return "Красный";
             case 2:
-                Orange = ColorNumber;
-                ColorName = "Оранжевый";
-                break;
+                return "Оранжевый";
             case 3:
-                Yellow = ColorNumber;
-                ColorName = "Желтый";
-                break;
+                return "Желтый";
             case 4:
-                Green = ColorNumber;
-                ColorName = "Зеленый";
-                break;
+                return "Зеленый";
             case 5:
-                Blue = ColorNumber;
-                ColorName = "Голубой";
-                break;
+                return "Голубой";
             case 6:
-                Siniy = ColorNumber;
-                ColorName = "Синий";
-                break;
+                return "Синий";
             case 7:
-                Purple = ColorNumber;
-                ColorName = "Фиолетовый";
-                break;
+                return "Фиолетовый";
             default:
-                UnknownColor = ColorNumber;
-                ColorName = "неизвестно";
+                return "неизвестно";
         }
-        return ColorName;
     }
 }
