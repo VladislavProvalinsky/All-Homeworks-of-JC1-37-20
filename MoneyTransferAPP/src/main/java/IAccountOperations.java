ee5a42c5db541163753f4c64ced09e150860fb90
VladislavProvalinsky <62176491+VladislavProvalinsky@users.noreply.github.com>
2020-06-01 12:19:07 +0300

    Add files via upload

--- MoneyTransferAPP/src/main/java/IAccountOperations.java
+++ MoneyTransferAPP/src/main/java/IAccountOperations.java
@@ -4,7 +4,7 @@ public interface IAccountOperations {
     public void withdrawBankAccount (ECurrency currency, double amount);
     public void depositPersonAccount (String IBAN, double amount);
     public void withdrawPersonAccount (String IBAN, double amount);
-    public void transferBankToBank (Account acc1, Account acc2, double amount);
-    public void transferP2PWithID (String personID_From, String personID_To, double amount);
+    public void transferBankToBank (Account from, Account to, double amount);
+    public void transferP2PWithID (String personIBAN_From, String personIBAN_To, double amount);
 
 }
